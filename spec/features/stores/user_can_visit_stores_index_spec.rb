8afd02947ff320f8dc2810030d12b903dfc408d7
amhursh <amhursh@gmail.com>
2017-10-17 23:40:21 -0600

    Fix test

--- spec/features/stores/user_can_visit_stores_index_spec.rb
+++ spec/features/stores/user_can_visit_stores_index_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 feature "User visits stores index" do
   scenario "and can see all stores" do
     create_list(:store, 5)
-    first_store = Store.first.update(name: "Charlie's Couch Corner")
-    last_store = Store.last.update(name: "Sweet Dee's Birdhouses")
+    first_store = Store.first
+    first_store.update(name: "Charlie's Couch Corner")
+    last_store = Store.last
+    last_store.update(name: "Sweet Dee's Birdhouses")
 
     visit '/'
 
     click_on 'View Stores'
 
-    expect(path).to eq stores_path
+    expect(current_path).to eq stores_path
 
     expect(page).to have_content first_store.name
     expect(page).to have_content last_store.name
