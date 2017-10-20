4aac7ccf023948b0d567f473ecb6e313919f875c
amhursh <amhursh@gmail.com>
2017-10-20 01:42:28 -0600

    Added reset action to passwords controller
    reset action finds user by email and calls confirmation sender's
    send_confirmation method with that user. Redirects to new confirmation
    path

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -3,4 +3,10 @@ class PasswordsController < ApplicationController
   def show
   end
 
+  def reset
+    user = User.find_by(email: params[:password][:email])
+    ConfirmationSender.send_confirmation(user)
+    redirect_to new_confirmation_path
+  end
+
 end
