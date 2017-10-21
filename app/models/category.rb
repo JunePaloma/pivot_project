b11ead6f352a93d2f3ce42116a728d148e34ca56
Becki Srofe <blf_23@yahoo.com>
2017-10-21 14:10:44 -0600

    Finish seeds

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,11 @@
 class Category < ApplicationRecord
-
-  has_many :items
+  has_many :category_items
+  has_many :items, through: :category_items
   validates :name, presence: :true, uniqueness: :true
 
   def image
     items.select('items.*')
-      .where("items.category_id = #{self.id}")
+       .where("category_items.category_id = #{self.id}")
       .order('RANDOM()')
       .limit(1)
       .first
