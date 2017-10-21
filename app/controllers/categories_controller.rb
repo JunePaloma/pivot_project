128964cacc27a4f2c7c7219565dcdfaae58641d0
Becki Srofe <blf_23@yahoo.com>
2017-10-21 15:25:44 -0600

    Fix test issue again

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
 
   def show
-    @category = Category.find(params[:category_name])
+    @category = Category.find_by_name(params[:category_name])
   end
 end
