40023bad792f24082bcf211f0cb9e6ad32306e4a
JunePaloma <kborkoski@gmail.com>
2017-10-22 11:32:04 -0600

    Add joins table between stores and operators

---
+++ app/models/store_operator.rb
@@ -0,0 +1,4 @@
+class StoreOperator < ApplicationRecord
+  belongs_to :store
+  belongs_to :operator
+end
--- db/migrate/20171022173139_create_store_operators.rb
+++ db/migrate/20171022173139_create_store_operators.rb
@@ -1,12 +1,8 @@
 class CreateStoreOperators < ActiveRecord::Migration[5.1]
   def change
     create_table :store_operators do |t|
-      t.string :name
-      t.string :user_name
       t.references :store, foreign_key: true
-      t.integer :role, default: 0
-      t.string :email
-      t.string :password_digest
+      t.references :operator, foreign_key: true
 
       t.timestamps
     end
