fffc977db466ea5ae3a6bd40642450449990d46e
JunePaloma <kborkoski@gmail.com>
2017-10-22 09:44:13 -0600

    Add sad path test for platform admin log in

--- spec/features/platform_admin/platform_admin_can_log_in_and_out_spec.rb
+++ spec/features/platform_admin/platform_admin_can_log_in_and_out_spec.rb
@@ -23,6 +23,24 @@ feature "Platform Admin can log in and out" do
     expect(current_path).to eq(root_path)
     expect(page).to_not have_link("Logout")
     expect(page).to have_content("Login")
+  end
 
+  scenario "sad path platform admin log in" do
+    user = User.create(username: "NotAPlatformAdmin", password: "password")
+
+    visit '/'
+
+    click_on("Login")
+
+    expect(current_path).to eq(root_path)
+
+    fill_in "session[username]", with: user.username
+    fill_in "session[password]", with: user.password
+
+    click_on("Log In")
+    visit platform_admin_dashboard_path
+
+    expect(current_path).not_to eq(platform_admin_dashboard_path)
+    expect(page).to have_content("404")
   end
 end
