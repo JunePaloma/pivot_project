39f8c68db5f6aebeb886f6debe762a1452c827d5
JunePaloma <kborkoski@gmail.com>
2017-10-22 09:55:38 -0600

    Add instance method for determining store admin

--- app/models/store_operator.rb
+++ app/models/store_operator.rb
@@ -2,4 +2,8 @@ class StoreOperator < ApplicationRecord
   has_secure_password
   belongs_to :store
   enum role: ["manager", "admin"]
+
+  def store_admin?
+    self.role == "admin"
+  end
 end
---
+++ spec/features/platform_admin/platform_admin_can_create_and_destroy_stores_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+featuer "Create and destroy stores" do
+  scenario "a registered platfrom admin" do
+
+    end
+  end
