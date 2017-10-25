8945962dafe687736f49a2507cc0ea22522ab193
JunePaloma <kborkoski@gmail.com>
2017-10-24 19:22:54 -0600

    Operators can modify their accounts through the dashboard

--- spec/features/admin/admin_can_modify_account_info_spec.rb
+++ spec/features/admin/admin_can_modify_account_info_spec.rb
@@ -24,30 +24,26 @@ feature "as a logged in admin" do
     allow_any_instance_of(ApplicationController).to receive(
     :current_operator).and_return(admin)
 
-    visit '/admin/dashboard'
-
-    click_on "Edit My Account"
-
-    expect(current_path).to eq(edit_admin_operator_path(admin))
+    visit edit_admin_operator_path(admin)
 
     fill_in "operator[user_name]", with: "Bill"
     fill_in "operator[name]", with: "Frankie"
 
     click_on "Update"
 
-    expect(current_path).to eq(edit_admin_user_path(admin))
-    expect(page).to have_content("Update failed")
+    expect(current_path).to eq(admin_dashboard_index_path)
+
   end
 
-  xscenario "I cant modify other user data" do
+  scenario "I can modify other operator data" do
     admin   = create(:operator, role: 1)
     operator  = create(:operator)
 
     allow_any_instance_of(ApplicationController).to receive(:current_operator).and_return(admin)
 
-    visit "/admin/operator/#{operator.id}/edit"
+    visit "/admin/operators/#{operator.id}/edit"
 
-    expect(page).to have_content("The page you were looking for doesn't exist")
+  #expectation TBD
   end
 
 
