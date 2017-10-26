b3173d832a41ae0f651a59e87ec26dc884937998
Becki Srofe <blf_23@yahoo.com>
2017-10-26 11:02:49 -0600

    Add pagination to user dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,17 +3,17 @@ class Admin::DashboardController < Admin::BaseController
   def index
     @all_orders = Order.all
     if params[:order_status] == "all"
-      @orders = Order.all.sort
+      @orders = Order.sort.paginate(:page => params[:page], :per_page => 10)
     elsif params[:order_status] == "completed"
-      @orders = Order.completed.sort
+      @orders = Order.completed.sort.paginate(:page => params[:page], :per_page => 10)
     elsif params[:order_status] == "paid"
-      @orders = Order.paid.sort
+      @orders = Order.paid.sort.paginate(:page => params[:page], :per_page => 10)
     elsif params[:order_status] == "cancelled"
-      @orders = Order.cancelled.sort
+      @orders = Order.cancelled.sort.paginate(:page => params[:page], :per_page => 10)
     elsif params[:order_status] == "ordered"
-      @orders = Order.ordered.sort
+      @orders = Order.ordered.sort.paginate(:page => params[:page], :per_page => 10)
     else
-      @orders = Order.all
+      @orders = Order.paginate(:page => params[:page], :per_page => 10)
     end
   end
 
