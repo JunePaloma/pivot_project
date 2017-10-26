d58bde5b11b736d2d10ca52bc984781a61ada607
amhursh <amhursh@gmail.com>
2017-10-25 20:06:34 -0600

    Order rework functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024024137) do
+ActiveRecord::Schema.define(version: 20171026003549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 20171024024137) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "store_id"
+    t.index ["store_id"], name: "index_orders_on_store_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -117,6 +119,7 @@ ActiveRecord::Schema.define(version: 20171024024137) do
   add_foreign_key "item_orders", "orders"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "stores"
+  add_foreign_key "orders", "stores"
   add_foreign_key "orders", "users"
   add_foreign_key "store_operators", "operators"
   add_foreign_key "store_operators", "stores"
