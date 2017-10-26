190999a238cd7832221a96d429dd779cd6b1639d
Becki Srofe <blf_23@yahoo.com>
2017-10-26 11:11:41 -0600

    Fix test

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,15 +3,15 @@ class Admin::DashboardController < Admin::BaseController
   def index
     @all_orders = Order.all
     if params[:order_status] == "all"
-      @orders = Order.sort.paginate(:page => params[:page], :per_page => 10)
+      @orders = Order.paginate(:page => params[:page], :per_page => 10)
     elsif params[:order_status] == "completed"
-      @orders = Order.completed.sort.paginate(:page => params[:page], :per_page => 10)
+      @orders = Order.completed.paginate(:page => params[:page], :per_page => 10)
     elsif params[:order_status] == "paid"
-      @orders = Order.paid.sort.paginate(:page => params[:page], :per_page => 10)
+      @orders = Order.paid.paginate(:page => params[:page], :per_page => 10)
     elsif params[:order_status] == "cancelled"
-      @orders = Order.cancelled.sort.paginate(:page => params[:page], :per_page => 10)
+      @orders = Order.cancelled.paginate(:page => params[:page], :per_page => 10)
     elsif params[:order_status] == "ordered"
-      @orders = Order.ordered.sort.paginate(:page => params[:page], :per_page => 10)
+      @orders = Order.ordered.paginate(:page => params[:page], :per_page => 10)
     else
       @orders = Order.paginate(:page => params[:page], :per_page => 10)
     end
