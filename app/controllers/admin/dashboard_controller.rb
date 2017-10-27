fa1183dc43dac565490e56895706601b2a5c5c28
Becki Srofe <blf_23@yahoo.com>
2017-10-26 19:18:02 -0600

    Delete method we didn't need anymore

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,6 @@
 class Admin::DashboardController < Admin::BaseController
 
   def index
-    @all_orders = Order.all
     if current_operator.role == "platform_admin"
       @orders = filter_orders(params[:order_status])
     else
