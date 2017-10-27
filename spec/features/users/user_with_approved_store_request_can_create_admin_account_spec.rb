960132bd17b93d25383dcd0ffd032fa163e170c9
amhursh <amhursh@gmail.com>
2017-10-26 21:23:20 -0600

    Completed store requests functionality

--- spec/features/users/user_with_approved_store_request_can_create_admin_account_spec.rb
+++ spec/features/users/user_with_approved_store_request_can_create_admin_account_spec.rb
@@ -30,4 +30,24 @@ describe 'User who has an approved store request can create an admin account' do
     expect(current_path).to eq admin_dashboard_index_path
     expect(assigned_store.operators.last.name).to eq user.name
   end
+
+  scenario 'and user without approved store requests cannot create an account' do
+    user = create(:user)
+    sr1 = create(:store_request, user: user, status: 'declined')
+    store = create(:store, name: sr1.name, description: sr1.description, slug: sr1.name.parameterize)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/dashboard'
+
+    click_on 'View Store Requests'
+
+    expect(page).to_not have_content("Approved")
+    expect(page).to_not have_content("Create Admin Account")
+
+    visit new_admin_operator_path
+
+    expect(page).to have_content("404")
+    expect(page).to_not have_content("Create Account")
+  end
 end
