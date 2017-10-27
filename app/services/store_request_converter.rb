cb7c64cef663ea0843b256058369f2a2109069df
amhursh <amhursh@gmail.com>
2017-10-26 21:24:01 -0600

    Remove comments

--- app/services/store_request_converter.rb
+++ app/services/store_request_converter.rb
@@ -14,7 +14,6 @@ class StoreRequestConverter
   def approved
     store = Store.new(name: name, description: description, slug: slug)
     if store.save
-      # upgrade_user(store)
       request.approved!
       return true
     end
@@ -30,21 +29,4 @@ class StoreRequestConverter
   def slug
     name.parameterize
   end
-
-  # def upgrade_user(store)
-  #   if attached_user.class == User
-  #     op = Operator.create(name: attached_user.name,
-  #                          user_name: attached_user.username,
-  #                          email: attached_user.email,
-  #                          password_digest: attached_user.password_digest)
-  #   else
-  #     op = attached_user
-  #   end
-  #   link_to_store(store, op)
-  # end
-
-  # def link_to_store(store, operator)
-  #   StoreOperator.create(store_id: store.id, operator_id: operator.id)
-  # end
-
 end
